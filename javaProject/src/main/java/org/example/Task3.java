226716cd21a5b3336eb004465cc10421dfb721c5
jmcox2099 <57116316+jmcox2099@users.noreply.github.com>
2024-11-06 14:05:33 -0500

    Add files via upload
    Justin 1st commit

---
+++ javaProject/src/main/java/org/example/Task3.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Task3 {
+    public static void main(String[] args) {
+        /*3. Create a 2D array or integer type where you will store odd and even numbers.
+        Develop a program that will identify/print the even numbers only.
+         */
+    }
+}
